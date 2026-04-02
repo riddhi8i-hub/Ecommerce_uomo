9c3f27e22b5e9ff86f613df2686374e99e5fca5a
Riddhiman <riddhimansaha8i@gmail.com>
2026-04-02 11:28:50 +0530

    Update ShopDetails.jsx

--- frontend/src/Components/Shop/ShopDetails/ShopDetails.jsx
+++ frontend/src/Components/Shop/ShopDetails/ShopDetails.jsx
@@ -161,7 +161,7 @@ const ShopDetails = () => {
                           <h5>{product.productName}</h5>
                         </Link>
 
-                        <p>${product.productPrice}</p>
+                        <p>₹{product.productPrice}</p>
                         <div className="sdProductRatingReviews">
                           <div className="sdProductRatingStar">
                             <FaStar color="#FEC78A" size={10} />
